311d30b4acd1de36beaeb2e7332385294221531e
tejudharanesh <tejudharanesh1234@gmail.com>
2024-10-26 17:59:33 +0530

    added required files

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js"; // Include .js extension
+import { createUser, getProfile } from "../controllers/userController.js"; // Include .js extension
 
 const userRouter = express.Router();
 
 // Create a new user
 userRouter.post("/push", createUser);
+userRouter.post("/getProfile", getProfile);
 
 export default userRouter; // Use 'export default' instead of module.exports
